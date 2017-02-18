99c7f32b469abf9ba7c37928bedf8c77251838a6
Hoai Nam <namptit307@gmail.com>
2017-02-19 00:15:08 +0700

    Update new mechanism

--- config.py
+++ config.py
@@ -1,14 +1,54 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
+# Server Specific Configurations
+server = {
+    'port': '3007',
+    'host': '0.0.0.0'
+}
 
-# using sqlite as database
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app.db'
-# log level
-LOG_LEVEL = 'INFO'
-# log file
-LOG_FILE = '/tmp/shortener.log'
-# Rate limit. Syntax should follow goo.gl/FWxPrF
-RATE_LIMIT = '200/hour;15/minute'
+# Pecan Application Configurations
+app = {
+    'root': 'shortener.controllers.root.RootController',
+    'modules': ['shortener'],
+    'static_root': '%(confdir)s/public',
+    'template_path': '%(confdir)s/shortener/templates',
+    'debug': True,
+    'errors': {
+        404: '/error/404',
+        '__force_dict__': True
+    }
+}
 
+logging = {
+    'root': {'level': 'INFO', 'handlers': ['console']},
+    'loggers': {
+        'shortener': {'level': 'DEBUG', 'handlers': ['console'], 'propagate': False},
+        'pecan': {'level': 'DEBUG', 'handlers': ['console'], 'propagate': False},
+        'py.warnings': {'handlers': ['console']},
+        '__force_dict__': True
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'color'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        },
+        'color': {
+            '()': 'pecan.log.ColorFormatter',
+            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
+                       '[%(threadName)s] %(message)s'),
+        '__force_dict__': True
+        }
+    }
+}
+
+# Custom Configurations must be in Python dictionary format::
+#
+# foo = {'bar':'baz'}
+#
+# All configurations are accessible at::
+# pecan.conf
